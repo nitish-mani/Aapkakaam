1fdbd97bf4b3b5c6df97beccaa881431a2a5006e
nitish-mani <nitishmani63@gnail.com>
2024-03-05 14:05:28 +0530

    changes

--- src/utils/base.jsx
+++ src/utils/base.jsx
@@ -1,6 +1,1 @@
-export const SERVER_URL =
-<<<<<<< HEAD
-  "http://localhost:3000";
-=======
-  "https://13.234.75.3:3000";
->>>>>>> b10c286f3e3ba4863f5871f89a367abe3e1ca9cc
+export const SERVER_URL = "http://localhost:3000";
